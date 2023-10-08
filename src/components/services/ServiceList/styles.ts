eec21290963b48cfc8d0a54c8f5b4a21773aa394
whizzie99 <wisdomobiomadev@gmail.com>
2023-10-08 12:06:29 +0100

    create responsive styles for services page

--- src/components/services/ServiceList/styles.ts
+++ src/components/services/ServiceList/styles.ts
@@ -2,17 +2,26 @@
 import styled from "styled-components";
 
 export const StyledWrapper = styled.div`
-  padding: 8em 0;
+  padding: 5em 0;
+
+  @media screen and (min-width: 1200px) {
+    padding: 8em 0;
+  }
 `;
 
 export const StyledShortTxt = styled.p`
   margin: 2em auto;
-  width: 50%;
+  width: 90%;
   text-align: justify;
   font-family: var(--font-alegreya);
-  font-size: 1.1em;
+  font-size: 1em;
   line-height: 1.5;
   font-weight: 300;
+
+  @media screen and (min-width: 1200px) {
+    width: 50%;
+    font-size: 1.1em;
+  }
 `;
 
 export const StyledServicesGrid = styled.div`
