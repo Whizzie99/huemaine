a779c12c9fbb8c292adb8b81708f5dd091f4cd51
whizzie99 <wisdomobiomadev@gmail.com>
2023-10-15 10:06:12 +0100

    remove og image

--- src/app/blog/[...id]/page.tsx
+++ src/app/blog/[...id]/page.tsx
@@ -11,13 +11,16 @@ export async function generateMetadata({ params }: { params: { id: string } }) {
   const data = await client.fetch(query, { next: { revalidate: 0 } });
 
   const post = data;
+
+
+  // console.log(urlFor(post.img).url());
   return {
     title: `Huemain | Blog - ${post.title}`,
     description: `${post.title?.substring(0, 30)}...`,
     openGraph: {
       title: `Huemain | Blog - ${post.title}`,
       description: `${post.title?.substring(0, 30)}...`,
-      images: [`${urlFor(post.img).url()}`],
+      // images: [`${urlFor(post?.img).url()}`],
     },
   };
 }
